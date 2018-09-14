a0607086ba9cfd7048984aa2daed8e5b48c3f1bd
Brent Kuzmanich <brentkuzmanich@gmail.com>
2018-09-13 20:31:06 -0700

    working on structure.

---
+++ BeachCabinReservation/wwwroot/source/global.js
@@ -0,0 +1,8 @@
+﻿window.App = window.App || {};
+var app = window.App;
+
+//namespaces
+app.Utility = app.Utility || {};
+app.Services = app.Services || {};
+app.Models = app.Models || {};
+
---
+++ BeachCabinReservation/wwwroot/source/home/index.js
@@ -0,0 +1,12 @@
+﻿
+(function ($, util) {
+    var name = "Index";
+
+    $(function () {
+        util.CheckDependencies(name, arguments);
+
+
+
+    });
+
+})(jQuery, window.App.Utility);
