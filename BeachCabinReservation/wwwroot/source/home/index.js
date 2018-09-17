6a2ec6ffd43467cb3f9b856ca19b250835d396ce
Brent Kuzmanich <brentkuzmanich@gmail.com>
2018-09-16 19:56:54 -0700

    Playing with FullCalendar options

--- BeachCabinReservation/wwwroot/source/home/index.js
+++ BeachCabinReservation/wwwroot/source/home/index.js
@@ -1,12 +1,47 @@
 ﻿
-(function ($, util) {
+(function ($, util, moment) {
     var name = "Index";
+    util.CheckDependencies(name, arguments); 
 
-    $(function () {
-        util.CheckDependencies(name, arguments);
+    $(function () {              
 
-
+        $("#calendar").fullCalendar({
+            defaultView: "month",
+            header: {
+                left: 'title',
+                center: 'addEvent',
+                right: 'agendaWeek,month today prev,next'
+            },
+            customButtons: {
+                addEvent: {
+                    text: "New Reservation",
+                    click: function () {
+                        alert("reserving")
+                    }
+                }
+            },
+            dayClick: function (day, jsEvent, view) {
+                if (view.name == "month") {
+                    $("#calendar").fullCalendar('changeView', 'agendaWeek');
+                }
+                $("#calendar").fullCalendar("gotoDate", day);
+            },
+            events: [
+                {
+                    title: "event 1",
+                    start: "2018-09-25",
+                    allDay: true
+                },
+                {
+                    title: "event 2",
+                    start: "2018-09-18T10:00:00",
+                    end: "2018-09-20T12:00:00"
+                }
+            ]
+        })
 
     });
 
-})(jQuery, window.App.Utility);
+
+
+})(jQuery, window.App.Utility, moment);
